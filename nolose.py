34be7fafb6b0cc5b364f4470c252594f6b79f4a3
d4n-1D08 <danirera35@gmail.com>
2025-02-18 20:55:32 -0600

    Mis proyectos
    mis proyectos

--- nolose.py
+++ nolose.py
@@ -1,7 +1,9 @@
 import random
-elementos = ("963214-addanilu")
+elementos = ("963214-addanilujhvyjyg")
+pass_lenght = int(input("Introduzca la longitud de su contraseña: "))
 
 pasword = ""
 for i in range (pass_length):
-     password+=random.choice(elementos)
-     print (password)
+    password += random.choice(elementos) # Se concatena el caracter a la variable "pasword"
+
+print ("Contraseña generada:", password)S
